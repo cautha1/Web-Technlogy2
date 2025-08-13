0bb6410bcbb79239894c86cbe37f7b74eb53ffe5
Cauthan <152834737+cautha1@users.noreply.github.com>
2025-08-13 15:17:34 +0930

    added folder

---
+++ AOWebApp/AOWebApp/Models/CodeFirst/ExampleItem.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AOWebApp.Models.CodeFirst
+{
+   
+    public class ExampleItem
+
+    {
+        [Key]
+        public int itemID { get; set; }
+
+        [Required]
+        [Display(Name ="Item Name")]
+        [StringLength(100, ErrorMessage ="The item musct be less than 100 characters")]
+        public string  itemName { get; set; } = string.Empty;
+
+        
+        [Range(1,100000, ErrorMessage ="The {0} must be between {1} and {2}")]
+        [Display(Name ="Price")]
+        [Required(ErrorMessage ="You must provide an item price")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="decimal(8, 2")]
+        public decimal itemPrice { get; set; }
+
+    }
+}
---
+++ AOWebApp/AOWebApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AOWebApp.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AOWebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AOWebAppContext") ?? throw new InvalidOperationException("Connection string 'AOWebAppContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting(); 
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+//Created git repo
